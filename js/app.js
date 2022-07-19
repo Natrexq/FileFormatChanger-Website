3981ab69341f0db464e1bef5276e5ba4756dcedc
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-07-19 19:25:17 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,11 +9,17 @@ function App(){
 
 
 
-
+const gtbut = document.getElementById('gtbut');
 const refpg = document.getElementById('flref');
+
     refpg.addEventListener('click', function Ref(){
 
         location.reload();
+
+    });
+    gtbut.addEventListener('click', function Project(){
+
+        window.open("https://github.com/Natrexq/FileFormatChanger-Website");
 
     });
 }
