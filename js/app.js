31aedf6565f917e7e442223c2d50255a6c28b03e
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-07-19 18:48:02 +0200

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function App(){
+
+
+
+
+
+
+
+
+
+
+
+const refpg = document.getElementById('flref');
+    refpg.addEventListener('click', function Ref(){
+
+        location.reload();
+
+    });
+}
+window.addEventListener('load', App, false);
