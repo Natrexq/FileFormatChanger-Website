df8740930930c844f61c2625596271648043e926
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-07-28 23:21:20 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,9 +8,36 @@ function App(){
 
 
 
+//Buttons from navbar
 
+const git = document.getElementById('gitn');
+const info = document.getElementById('info');
+const auth = document.getElementById('auth');
+
+git.addEventListener('click', function Do(){
+
+    window.open("https://github.com/Natrexq/FileFormatChanger-Website");
+
+});
+info.addEventListener('click',function Do(){
+
+    window.open("https://github.com/Natrexq/FileFormatChanger-Website/blob/main/README.md");
+
+
+});
+auth.addEventListener('click',function Do(){
+
+    window.open("https://github.com/Natrexq");
+
+
+});
+
+
+//
 const gtbut = document.getElementById('gtbut');
 const refpg = document.getElementById('flref');
+
+
 
     refpg.addEventListener('click', function Ref(){
 
