70b955a251d1497be5649bb2c82d6d132e25f5f1
Natrexq Natrexq <natanot.yt@gmail.com>
2022-08-14 14:24:07 +0200

    small changes

--- js/app.js
+++ js/app.js
@@ -50,4 +50,31 @@ const refpg = document.getElementById('flref');
 
     });
 }
+function Credits(){
+    
+    
+
+
+
+    //
+
+    const ig = document.getElementById('ig');
+    const fb = document.getElementById('fb');
+    const git = document.getElementById('gt');
+    const twt = document.getElementById('twt');
+
+    // I will add fb and insta in futuer
+
+    function Git(){window.open("https://github.com/Natrexq");}
+    function Twt(){window.open("https://twitter.com/natrexq");}
+    function Soon(){alert("Soon...");}
+
+    fb.addEventListener('click', Soon,false);
+    ig.addEventListener('click', Soon,false);
+    git.addEventListener('click', Git,false);
+    twt.addEventListener('click', Twt,false);
+    
+
+}
+window.addEventListener('load', Credits, false);
 window.addEventListener('load', App, false);
